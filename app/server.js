ae11b103912ccb96846eb0073fe2c0bc3dd279b2
Aravindh S N R <aravindhsnr@gmail.com>
2016-11-26 00:39:50 +0530

    Setting views location

--- app/server.js
+++ app/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', 'app/views');
 
 app.use(express.static('./public'));
 app.use(require('./routes/index.js'));
